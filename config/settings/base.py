c14c73dc19f298c448819dbe512b8ac3f969ab83
Shaojun Zhu <shj.zhu@gmail.com>
2020-05-13 18:43:30 +0800

    Implement cheese country of origin

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,6 +77,7 @@ THIRD_PARTY_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "django_countries",
 ]
 
 LOCAL_APPS = [
--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -6,6 +6,7 @@ pytestmark = pytest.mark.django_db
 from ..models import Cheese
 from .factories import CheeseFactory
 
+
 def test___str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
